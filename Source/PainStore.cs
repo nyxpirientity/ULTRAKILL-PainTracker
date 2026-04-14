d1c1f960e2c6c4f460a91376cb6ff3f1343e9784
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-04-14 12:19:07 -0700

    massively reduce pain rot

--- Source/PainStore.cs
+++ Source/PainStore.cs
@@ -127,7 +127,7 @@ namespace Nyxpiri.ULTRAKILL.PainTracker
 
         protected void FixedUpdate()
         {
-            float painRot = 2.5f + (Mathf.Pow(NumPainPerceptors, 1.05f)) * 0.5f;
+            float painRot = 2.5f + (Mathf.Pow(NumPainPerceptors, 1.05f)) * 0.15f;
             painRot = Mathf.Max(0.0f, painRot);
             AddPain(-painRot * Time.fixedDeltaTime);
         }
